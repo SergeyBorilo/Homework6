765a2e36e4d034a613e1cb37bf15956d705744df
SergeyBorilo <S_Barya@i.ua>
2024-12-21 19:32:48 +0200

    Homework6

---
+++ Homework6/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Homework6;
+
+
+using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter the number in decimal format: ");
+
+        if (int.TryParse(Console.ReadLine(), out var number) && number >= 0)
+        {
+            var binary = Convert.ToString(number, 2);
+            Console.WriteLine($"Binary representation of a number {number}: {binary}");
+
+            var divisibleByTwo = "0$";
+            if (Regex.IsMatch(binary, divisibleByTwo))
+                Console.WriteLine($"The number {binary} is divisible by 2 without remainder");
+            else
+                Console.WriteLine($"The number {binary} is not divisible by 2");
+        }
+        else
+            Console.WriteLine("Enter a valid positive integer: ");
+    }
+}
